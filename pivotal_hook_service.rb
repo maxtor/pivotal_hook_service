52757cec8b3f9cef68fce419d66d32d9e2df4afd
Maxim Bondaruk <maxim.bondaruk@railsware.com>
2010-11-02 01:43:10 +0200

    [#5974864] Ready for rack

--- pivotal_hook_service.rb
+++ pivotal_hook_service.rb
@@ -2,29 +2,32 @@ require 'sinatra'
 require 'xmlsimple'
 require 'logger'
 
-configure(:development) do |c|
-  require "sinatra/reloader"
-end
-
-configure do
-  LOGGER = Logger.new("messages.log") 
-end
+class PivotalHookService < Sinatra::Base
  
-helpers do
-  def logger
-    LOGGER
+  configure do
+    LOGGER = Logger.new("log/messages.log") 
   end
-end
-
-post '/monkey/:chat_blob' do
-  xml = request.body.read
+ 
+  helpers do
+    def logger
+      LOGGER
+    end
+  end
   
-  doc = XmlSimple.xml_in(xml)
-  case doc["event_type"].to_s
-  when "story_create", "note_create", "story_update"
-    message = doc["description"]
-  else
-    message = "ubdefined"
+  get '/hello' do
+    "hello"
   end
-  logger.info "Chat: #{params[:chat_blob]}, message #{message}"
+
+  post '/monkey/:chat_blob' do
+    xml = request.body.read
+  
+    doc = XmlSimple.xml_in(xml)
+    case doc["event_type"].to_s
+    when "story_create", "note_create", "story_update"
+      message = doc["description"]
+    else
+      message = "ubdefined"
+    end
+    logger.info "Chat: #{params[:chat_blob]}, message #{message}"
+  end
 end
